365c981fbf89ed97c74302e633622508d6a98104
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-07-17 22:12:05 +0300

    Добавил тест, который проверяет правильность вычитания. #1

--- TestProjectCalculator/UnitTestOperation.cs
+++ TestProjectCalculator/UnitTestOperation.cs
@@ -24,6 +24,24 @@ namespace TestProjectCalculator
         }
 
         [Theory]
+        [InlineData(21, 3, 5)]
+        [InlineData(0, 2, 5)]
+        public void Subtraction_To_Value_Operand_Number_Iterations(decimal valueResult, decimal valueOperand, int NumberIterations)
+        {
+            var mathCalculator = new MathCalculator();
+
+            var expectResult = valueResult - (NumberIterations * valueOperand);
+
+
+            for (var i = 0; i < NumberIterations; i++)
+            {
+                valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString(), '-');
+            }
+
+            Assert.Equal(expectResult, valueResult);
+        }
+
+        [Theory]
         [InlineData(5, 5, 3)]
         [InlineData(0, 2, 2)]
         public void Multiplication_To_Value_Operand_Number_Iterations(decimal valueResult, decimal valueOperand, int NumberIterations)
