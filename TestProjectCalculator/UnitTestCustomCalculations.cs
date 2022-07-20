2c7f6f7b4a0b3354b7ef7d5922724d0130a10f52
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-07-20 23:11:37 +0300

    Добавил тесты #1 для проверки операций: "-", "*", "/".

--- TestProjectCalculator/UnitTestCustomCalculations.cs
+++ TestProjectCalculator/UnitTestCustomCalculations.cs
@@ -8,7 +8,7 @@ namespace TestProjectCalculator
         [Theory]
         [InlineData(21, 5, 126)]
         [InlineData(0, 4, 0)]
-        public void Perform_Summing_Previous_Operand_When_Pressing_Equals(decimal valueResult, int numberIterations, decimal expectResult)
+        public void Perform_Summation_Previous_Operand_When_Pressing_Equals(decimal valueResult, int numberIterations, decimal expectResult)
         {
             var mathCalculator = new MathCalculator();
 
@@ -30,6 +30,109 @@ namespace TestProjectCalculator
             var mathCalculator = new MathCalculator();
 
             valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString(), '+');
+            valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString());
+
+            for (var i = 0; i < numberIterations - 1; i++)
+            {
+                valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty);
+            }
+
+            Assert.Equal(expectResult, valueResult);
+        }
+
+        [Theory]
+        [InlineData(21, 5, -84)]
+        [InlineData(0, 4, 0)]
+        public void Perform_Subtraction_Previous_Operand_When_Pressing_Equals(decimal valueResult, int numberIterations, decimal expectResult)
+        {
+            var mathCalculator = new MathCalculator();
+
+            valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty, '-');
+
+            for (var i = 0; i < numberIterations; i++)
+            {
+                valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty);
+            }
+
+            Assert.Equal(expectResult, valueResult);
+        }
+
+        [Theory]
+        [InlineData(21, 4, 6, -3)]
+        [InlineData(0, 3, 4, -12)]
+        public void Performing_Subtraction_Without_Operand_When_Pressing_Equals(decimal valueResult, decimal valueOperand, int numberIterations, decimal expectResult)
+        {
+            var mathCalculator = new MathCalculator();
+
+            valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString(), '-');
+            valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString());
+
+            for (var i = 0; i < numberIterations - 1; i++)
+            {
+                valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty);
+            }
+
+            Assert.Equal(expectResult, valueResult);
+        }
+
+        [Theory]
+        [InlineData(21, 1, 441)]
+        [InlineData(0, 4, 0)]
+        public void Perform_Multiplication_Previous_Operand_When_Pressing_Equals(decimal valueResult, int numberIterations, decimal expectResult)
+        {
+            var mathCalculator = new MathCalculator();
+
+            valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty, '*');
+
+            for (var i = 0; i < numberIterations; i++)
+            {
+                valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty);
+            }
+
+            Assert.Equal(expectResult, valueResult);
+        }
+
+        [Theory]
+        [InlineData(21, 3, 4, 1701)]
+        [InlineData(0, 3, 2, 0)]
+        public void Performing_Multiplication_Without_Operand_When_Pressing_Equals(decimal valueResult, decimal valueOperand, int numberIterations, decimal expectResult)
+        {
+            var mathCalculator = new MathCalculator();
+
+            valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString(), '*');
+            valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString());
+
+            for (var i = 0; i < numberIterations - 1; i++)
+            {
+                valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty);
+            }
+
+            Assert.Equal(expectResult, valueResult);
+        }
+
+        [Theory]
+        [InlineData(21, 2, 0.047619)]
+        public void Perform_Division_Previous_Operand_When_Pressing_Equals(decimal valueResult, int numberIterations, decimal expectResult)
+        {
+            var mathCalculator = new MathCalculator();
+
+            valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty, '/');
+
+            for (var i = 0; i < numberIterations; i++)
+            {
+                valueResult = mathCalculator.Eval(valueResult.ToString(), string.Empty);
+            }
+
+            Assert.Equal(expectResult, valueResult);
+        }
+
+        [Theory]
+        [InlineData(21, 3, 3, 0.777778)]
+        public void Performing_Division_Without_Operand_When_Pressing_Equals(decimal valueResult, decimal valueOperand, int numberIterations, decimal expectResult)
+        {
+            var mathCalculator = new MathCalculator();
+
+            valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString(), '/');
             valueResult = mathCalculator.Eval(valueResult.ToString(), valueOperand.ToString());
 
             for (var i = 0; i < numberIterations - 1; i++)
